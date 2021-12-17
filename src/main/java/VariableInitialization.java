f4b7269ca5ba84fa716639509bbbfd7d603aa140
Erselan Khan <arsalankhan994@gmail.com>
2021-12-17 15:30:22 +0500

    variable initialization example

---
+++ src/main/java/VariableInitialization.java
@@ -0,0 +1,39 @@
+public class VariableInitialization {
+
+    public static void main(String[] args) {
+
+        // String value
+        String stringValue = "Erselan Khan";
+
+        System.out.println("String value " + stringValue);
+
+        // Double value
+        Double doubleObjectvalue = 1.0;
+        double doubleValue = 1.0;
+
+        System.out.println("Double Object Value: " + doubleObjectvalue);
+        System.out.println("Double primitive type value " + doubleValue);
+
+        // Float Value
+        Float floatObjectValue = 1.0f;
+        float floatValue = 1.0f;
+
+        System.out.println("Float Object Value: " + floatObjectValue);
+        System.out.println("Float primitive type value " + floatValue);
+
+        // Boolean Value
+        Boolean booleanObjectValue = false;
+        boolean booleanValue = false;
+
+        System.out.println("Boolean Object Value: " + booleanObjectValue);
+        System.out.println("Boolean primitive type value " + booleanValue);
+
+        // Int Value
+        Integer integerObjectValue = 1;
+        int intValue = 1;
+
+        System.out.println("Integer Object Value: " + integerObjectValue);
+        System.out.println("Integer primitive type value " + intValue);
+
+    }
+}
