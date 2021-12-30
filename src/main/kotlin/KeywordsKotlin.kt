8de5256b5c98bc2b959055233c514fd350edb62e
Erselan Khan <arsalankhan994@gmail.com>
2021-12-30 17:29:46 +0500

    kotlin keywords example

---
+++ src/main/kotlin/KeywordsKotlin.kt
@@ -0,0 +1,73 @@
+fun main() {
+
+    /*
+    using val, to assign value only one time
+    */
+    val assignValueOnlyOneTime = "Erselan Khan"
+    assignValueOnlyOneTime = ""
+
+    /*
+    using var, to assign value multiple times
+    */
+    var assignValueMultipleTimes = "Erselan Khan"
+    assignValueMultipleTimes = "Arsalan Khan"
+
+    /*
+    Accessing method from class object
+    */
+    val keywordsKotlin = KeywordsKotlin()
+    keywordsKotlin.objectBoundMethod()
+
+    /*
+    calling method of singleton class
+    */
+    SingletonClass.methodOfSingletonClass()
+
+    /*
+    Accessing method from class
+    */
+    KeywordsKotlin.classBoundMethod()
+}
+
+class KeywordsKotlin {
+    companion object {
+        fun classBoundMethod() {
+            println("This is class bound method")
+        }
+    }
+
+    fun objectBoundMethod() {
+        println("This is object bound method")
+    }
+}
+
+class ChildClassOne : ExtendableClass() {
+
+}
+
+/*
+using open, allows subclassing a class or overriding a member
+*/
+open class ExtendableClass {
+
+}
+
+class ChildClassTwo : NonExtendableClass() {
+
+}
+
+/*
+by default all classes are non-extendable in Kotlin
+*/
+class NonExtendableClass {
+
+}
+
+/*
+using object, to make class Singleton
+*/
+object SingletonClass {
+    fun methodOfSingletonClass() {
+        println("This is a method of singleton class")
+    }
+}
